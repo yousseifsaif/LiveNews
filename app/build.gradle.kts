c67961f9627b3ae06d52d759ed1789b0038ea976
yousseif saif <prcomputer2@gmail.com>
2025-03-21 13:38:35 +0200

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_20
+        targetCompatibility = JavaVersion.VERSION_20
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "20"
     }
     buildFeatures {
         viewBinding = true
--- app/src/main/java/com/example/newsapp/SignupActivity.kt
+++ app/src/main/java/com/example/newsapp/SignupActivity.kt
@@ -43,28 +43,22 @@ class SignupActivity : AppCompatActivity() {
         } else if (email.isEmpty()) {
             binding.emailEd.error = "Please enter your email"
             shakeView(binding.emailEd)
-        }
-
-        else if (!email.matches(emailPattern)) {
+        } else if (!email.matches(emailPattern)) {
             binding.emailEd.error = "Only Gmail and Hotmail are allowed"
 
 
-
-        }
-
-        else if (password.isEmpty()) {
+        } else if (password.isEmpty()) {
             binding.passwordEd.error = "Please enter your password"
             shakeView(binding.passwordEd)
 
         } else if (password.length <= 8) {
             binding.passwordEd.error = "Password should be at least 8 characters"
             shakeView(binding.passwordEd)
-        }
-        else if (!password.matches(passwordPattern)) {
-            binding.passwordEd.error = "Password should contain at least one uppercase letter, one lowercase letter, one digit, and one special character"
+        } else if (!password.matches(passwordPattern)) {
+            binding.passwordEd.error =
+                "Password should contain at least one uppercase letter, one lowercase letter, one digit, and one special character"
             shakeView(binding.passwordEd)
-        }
-        else if (cpassword.isEmpty()) {
+        } else if (cpassword.isEmpty()) {
             binding.cpasswordEd.error = "Please enter your confirm password"
             shakeView(binding.cpasswordEd)
 
@@ -77,9 +71,7 @@ class SignupActivity : AppCompatActivity() {
             binding.nationalty.error = "Please enter your county"
             shakeView(binding.nationalty)
 
-        }
-
-        else {
+        } else {
 
             addUser(email, password, name, country)
         }
@@ -132,15 +124,17 @@ class SignupActivity : AppCompatActivity() {
 //        }
 //}
 
-    private fun dbAdd(email: String, name: String, country: String,) {
+    private fun dbAdd(email: String, name: String, country: String) {
         val u = user(name, email, country)
         db.collection("users")
             .add(u).addOnSuccessListener {
                 it.update("id", it.id)
             }
     }
+
     fun shakeView(view: View) {
-        val animator = ObjectAnimator.ofFloat(view, "translationX", 0f, 10f, -10f, 10f, -10f, 5f, -5f, 0f)
+        val animator =
+            ObjectAnimator.ofFloat(view, "translationX", 0f, 10f, -10f, 10f, -10f, 5f, -5f, 0f)
         animator.duration = 500
         animator.start()
     }
